1192a4aa751ee83f4bb5dfbed9d24f4e85a0a541
krishna korporu <krishnakorporu@gmail.com>
2018-01-25 19:13:08 +0530

    hello worldSpringsHelloWorld

---
+++ SpringsHelloWorld/src/main/java/com/ibm/SpringsDay1/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.ibm.SpringsDay1;
+/*...
+ * Author krishna Korporu
+ */
+public class HelloWorld {
+	
+	public void sayHello() {
+		System.out.println("Hello World...!");
+	}
+}
---
+++ SpringsHelloWorld/src/main/java/com/ibm/SpringsDay1/MainApp.java
@@ -0,0 +1,13 @@
+package com.ibm.SpringsDay1;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+	public static void main(String args[])
+	{
+	ApplicationContext context= new ClassPathXmlApplicationContext("beans.xml");
+	HelloWorld helloWorldBean=(HelloWorld) context.getBean("helloWorld");
+	helloWorldBean.sayHello();
+	}
+}
